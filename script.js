3a28845fa28c9c45fcd86393e0494ccf2c0ad53d
vbelovyii2 <50800995+vbelovyii2@users.noreply.github.com>
2019-07-26 12:49:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+   let slide = document.querySelector('.slide'); // получаем элемент с картинкой
+    //Пишем функцию замены классов для изменения картинки
+     let count = 0;
+    function changeClassSlid(){
+        let slides = ['first-slide','second-slide','third-slide']; //массив классов
+        slide.classList.remove(slides[0]); //удаляем все классы
+        slide.classList.remove(slides[1]);
+        slide.classList.remove(slides[2]);
+        count++;                                      //увеличиваем счетчик
+        slide.classList.add(slides[count]);//показываем класс согласно счетчику
+        if (count==2){ ///////Обнуляем счетчик
+            count=-1;
+        }
+        
+    }
+    setInterval(changeClassSlid,7000); //Меняем картинку каждые 7 секунд
+    //////////////////////////////////////////////////////////////////////////////////////////
+   ///////////это удалим потом 
+    let neucha = document.querySelector('.neucha');
+    let ud1 = document.querySelector('.ud1')
+    let ud2 = document.querySelector('.ud2')
+    
+    neucha.addEventListener('mouseover',function(event){
+       let target = event.target;
+        if (target&&target.classList.contains('ud')){
+            ud1.style.display = 'block';
+        }
+       if (target&&target.classList.contains('element-menu')){
+            ud2.style.display = 'block';
+        }   
+    });
+       neucha.addEventListener('mouseout',function(event){
+        let target = event.target;
+        if (target&&target.classList.contains('ud')){
+            ud1.style.display = 'none'}
+       if (target&&target.classList.contains('element-menu')){
+            ud2.style.display = 'none'}   
+        });
+////////////////////////////////////////////////////////////////////
+        
+})
